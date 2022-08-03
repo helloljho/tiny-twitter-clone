abfe1f1b6708ffd44d5a2ee5d4cb257da1ba79a0
helloljho@gmail.com <helloljho@gmail.com>
2022-08-03 13:49:38 +0900

    log out

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { authService } from '../fbase';
+import { useHistory } from 'react-router-dom';
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const history = useHistory();
+  const onLogoutClick = () => {
+    authService.signOut();
+    history.push('/');
+  };
+  return (
+    <>
+      <button onClick={onLogoutClick}>Log Out</button>
+    </>
+  );
 };
 
 export default Profile;
