79a2602c2f92cf212f710b3b776c5fa1a3707b50
helloljho@gmail.com <helloljho@gmail.com>
2022-08-03 14:16:00 +0900

    doing tweet

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { dbService } from '../fbase';
 
 const Home = () => {
-  return <div>Home</div>;
+  const [tweet, setTweet] = useState('');
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setTweet(value);
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection('tweet').add({
+      tweet,
+      createdAt: Date.now(),
+    });
+    setTweet('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="what's on your mind?"
+          onChange={onChange}
+          maxLength={120}
+          value={tweet}
+        />
+        <input type="submit" value="Tweet" />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
